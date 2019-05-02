9065490cef87e0f15605987fb23d3b9331710d21
Dimone_Lemone <eterniumsoul@gmail.com>
2019-05-02 13:54:24 +0300

    USART

--- usart/usart.h
+++ usart/usart.h
@@ -8,16 +8,31 @@
 
 #define UART_RX_BUFF 128
 
-/* @brief: function for controlling min and max encoder values and loop values after overflow
+/* @brief: init function for usart
  * @param: none
  * @retval: none
  */
 void usart_init(void);
+
+/* @brief: send byte via usart
+ * @param: byte to send
+ * @retval: none
+ */
 void send_byte(uint8_t byte);
+
+/* @brief: send string via usart using send_byte()
+ * @param: string to send
+ * @retval: none
+ */
 void send_string(char * string);
+
+/* @brief: USART RX handler
+ * @param: none
+ * @retval: none
+ */
 void USART3_IRQHandler(void);
 
-char uart_RX_buffer[UART_RX_BUFF];
-uint16_t uart_RX_buffer_pos;
+char uart_RX_buffer[UART_RX_BUFF];	// RX buffer
+uint16_t uart_RX_buffer_pos;		// RX buff position
 
 #endif
